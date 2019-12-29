1ac327ea554aeea4e234bdad75a6dc7c645c3780
Travis Elkins <travis.elkins+github@gmail.com>
2019-12-29 16:44:44 +0100

    Tracking/outputting elapsed time for task group to complete and tried flushing output.

--- app/Tasks/TaskGroup.php
+++ app/Tasks/TaskGroup.php
@@ -4,6 +4,8 @@ namespace App\Tasks;
 
 class TaskGroup
 {
+    private $start;
+    private $stop;
     private $tasks;
 
     public function __construct(string $title)
@@ -23,6 +25,8 @@ class TaskGroup
         $this->output('<hr>');
 
         $this->output('<ul>');
+
+        $this->start = now();
         $this->tasks->each(function ($task) {
             $task->run();
 
@@ -30,14 +34,25 @@ class TaskGroup
 
             $this->output("<li>{$task->title()} - {$status}</li>");
         });
+        $this->stop = now();
         $this->output('</ul>');
 
-        $this->output("<h1>{$this->title} - done</h1>");
+        $this->output("<h1>{$this->title} - done ({$this->elapsedTime()})</h1>");
         $this->output('<hr>');
     }
 
-    protected function output(string $output, bool $lineBreak = false, bool $newline = true)
+    protected function elapsedTime(): string
+    {
+        return $this->start->timespan($this->stop);
+    }
+
+    protected function output(string $output, bool $lineBreak = false, bool $newline = true, bool $flush = true)
     {
         echo $output . ($lineBreak ? '<br />' : '') . ($newline ? "\n" : '');
+
+        if ($flush) {
+            flush();
+            ob_flush();
+        }
     }
 }
